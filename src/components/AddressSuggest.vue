bf5b22d7027e373d14af558b53186529291b96e0
Hudson Sadlier <hudson.sadlier@portlandoregon.gov>
2026-02-10 23:28:03 -0800

    fix: hide svg expander from aria
    svg overlaps with button, so can be ignored since the button encompasses it
    
    ref: ess-1127

--- src/components/AddressSuggest.vue
+++ src/components/AddressSuggest.vue
@@ -30,7 +30,7 @@
             <option value="taxLot">Tax lot</option>
           </select>
           <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center">
-            <i v-html="feather.icons['chevron-down'].toSvg({ class: 'w-5 h-5' })" />
+            <i v-html="feather.icons['chevron-down'].toSvg({ class: 'w-5 h-5' })" aria-hidden="true" role="img" alt="" />
           </div>
         </div>
         <button aria-label="Search" class="p-2 rounded focus:outline-none focus:shadow-outline">
